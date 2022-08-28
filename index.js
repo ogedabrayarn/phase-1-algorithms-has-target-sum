1cf162f54245d2aa0ed739031a87ebccfbe91a60
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-08-28 10:37:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0; i < array.length; i++){
+    const pairSum = target - array[i]
+    for(let j = 0 ; j < array.length; j++){
+      if(array[j] === pairSum) return true;
+    }
+  }
+   return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -25,10 +33,19 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3))
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5))
+
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+  
+  console.log("Expecting: false");
+  console.log("=>", (hasTargetSum([4], 5)))
 }
 
 module.exports = hasTargetSum;
